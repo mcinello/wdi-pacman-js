54fe563a1aa0bc974e4cadb1a442792f051b378c
Michelle Cinello <m.cinello@gmail.com>
2017-11-07 17:04:35 -0500

    if user eats all 4 power pellets, the option p is removed from the menu

--- pacman.js
+++ pacman.js
@@ -62,7 +62,9 @@ function displayStats() {
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
-  console.log('(p) Eat Power-Pellet');
+  if (powerPellets > 0) {
+    console.log('(p) Eat Power-Pellet');
+  }
   console.log('(1) Eat Inky');
   console.log('(2) Eat Blinky');
   console.log('(3) Eat Pinky');
